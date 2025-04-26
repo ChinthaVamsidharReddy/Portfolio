04b957a1bcaf47639503e24b8859284577fa8378
Chintha Vamsidhar Reddy <vamsidharreddy831@gmail.com>
2025-04-26 23:21:30 +0530

    Fix Tailwind CSS configuration

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const nextConfig = {
   swcMinify: true,
   // next.config.js
   output: 'export', // This will make Next.js export static HTML
-  images: {
-    unoptimized: true,
-  },
   basePath: '/Portfolio',
   assetPrefix: '/Portfolio',
 }
